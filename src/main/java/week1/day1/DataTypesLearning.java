187f59447ed668094804c1fdf2d48fec5e366d42
Petchimuthu K <petchimuthu0891@gmail.com>
2023-08-19 22:05:18 +0530

    First Commit

---
+++ src/main/java/week1/day1/DataTypesLearning.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class DataTypesLearning {
+	public static void main(String[] args) {
+		int salpackage =40000;
+		boolean ismarried=true;
+		String uname="Petchimuthu";
+		System.out.println("My Age is "+salpackage);
+		System.out.println(ismarried);
+		System.out.println("user name is "+uname);
+		
+	}
+
+}
